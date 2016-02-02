b9f3b897e4f16291f8d2c53102f0ec7ea8d481dc
Michael Bailly <mbailly@linagora.com>
2016-02-02 22:52:17 +0100

    Library: listing by titles

--- app/components/library/library.js
+++ app/components/library/library.js
@@ -8,10 +8,35 @@ const app = angular.module('d10-library', ['d10-rest', 'op.dynamicDirective'])
     template: '<a ui-sref="main.listing.titles" href="">Songs by title</a>'
   };
 })
-.config(function (dynamicDirectiveServiceProvider) {
+.directive('d10LibraryListingTitles', function (d10API) {
+  function d10LibraryListingTitlesController() {
+    d10API.listSongsSortedByTitle()
+    .then((response) => {
+      this.songs = response;
+    });
+  }
+
+  return {
+    restrict: 'E',
+    templateUrl: 'components/library/listing-titles.html',
+    controller: d10LibraryListingTitlesController,
+    controllerAs: 'songsByTitle'
+  };
+
+})
+.config(function ($stateProvider, dynamicDirectiveServiceProvider) {
   let dd = new dynamicDirectiveServiceProvider.DynamicDirective(true, 'd10-songs-menu-entry');
 
   dynamicDirectiveServiceProvider.addInjection('side-menu-main-links', dd);
+
+  $stateProvider
+  .state('main.listing', {
+    url: '',
+    templateUrl: 'components/library/listing.html'
+  })
+  .state('main.listing.titles', {
+    template: '<d10-library-listing-titles></d10-library-listing-titles>'
+  });
 });
 
 export default app;
